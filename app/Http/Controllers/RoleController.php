0426d891459584fdc074e41584cae15ed58f414d
dovankhoa25 <khoado432@gmail.com>
2024-08-14 00:01:23 +0700

    role rs

---
+++ app/Http/Controllers/RoleController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\RoleResource;
+use App\Models\Role;
+use Illuminate\Http\Request;
+
+class RoleController extends Controller
+{
+    public function index()
+    {
+        $roles =  Role::all(); 
+        return response()->json([
+            'role' => new RoleResource($roles),
+        ], 201);
+    }
+
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|string|max:255',
+        ]);
+        $validatedData['status'] = true;
+
+
+        $role = Role::create($validatedData);
+
+        return response()->json([
+            'role' => new RoleResource($role),
+        ], 201);
+    }
+
+    public function show($id)
+    {
+        $role = Role::findOrFail($id);
+        return response()->json([
+            'role' => new RoleResource($role),
+        ], 201);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $role = Role::findOrFail($id);
+
+        $validatedData = $request->validate([
+            'name' => 'sometimes|string|max:255',
+            'status' => 'sometimes|boolean',
+        ]);
+    
+
+        $role->update($validatedData);
+    
+        return response()->json([
+            'role' => new RoleResource($role),
+        ], 201);
+    }
+
+    public function destroy($id)
+    {
+        $role = Role::findOrFail($id);
+        $role->delete();
+
+        return response()->json(null, 204); 
+        
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,25 +1,27 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\RoleController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
+GET /roles - index
+GET /roles/{id} - show
+POST /roles - store
+PUT/PATCH /roles/{id} - update
+DELETE /roles/{id} - destroy
+
 */
 
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
+
+// user
 Route::post('register', [AuthController::class, 'register'])->name('api.register');
-
 Route::post('login', [AuthController::class, 'login'])->name('api.login');
-
 Route::get('user', [AuthController::class, 'getUser'])->middleware('auth');
+
+// roles
+Route::apiResource('roles', RoleController::class);
