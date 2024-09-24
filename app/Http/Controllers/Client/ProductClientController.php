6c219d3c9efba83b16c4a948937d713d91d8b90e
dovankhoa25 <khoado432@gmail.com>
2024-09-24 10:11:13 +0700

    updata

--- app/Http/Controllers/Client/ProductClientController.php
+++ app/Http/Controllers/Client/ProductClientController.php
@@ -6,63 +6,190 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\Product\FilterProductRequest;
 use App\Http\Resources\ProductResource;
 use App\Models\Product;
+use App\Models\ProductDetail;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Request;
 
 class ProductClientController extends Controller
 {
-    public function getProduct(Request $request){
+    public function getProduct(FilterProductRequest $request)
+    {
+        $perPage = $request->input('per_page', 10);
+        $products = Product::with(['productDetails'])
+            ->Filter($request)->paginate($perPage);
 
-            $perPage = $request['per_page'] ?? 10;
-
-            $products = Product::paginate($perPage);
-
+        if ($products->isEmpty()) {
             return response()->json([
-                'data' => $products,
-            ],201);
-       
-    }
-    
+                'message' => 'Không có sản phẩm nào',
+            ], 404);
+        }
 
+        $formattedProducts = collect($products->items())->map(function ($product) {
+            $minPrice = $product->productDetails->min('price');
+            $maxPrice = $product->productDetails->max('price');
 
-    public function getProductAllWithDetail(Request $request){
-
-            $perPage = $request['per_page'] ?? 10;
-
-            $products = $products = Product::with(['productDetails.images.sizes'])->paginate($perPage);
-
-            return response()->json([
-                'data' => $products,
-            ],201);
-
-    }
-
-
-
-    public function getProductWithDetailByID(int $id){
-
-        $products = Product::with(['productDetails.images.sizes.category'])
-        ->where('id' ,'=',$id)
-        ->first();
+            return [
+                'id' => $product->id,
+                'name' => $product->name,
+                'category' => [
+                    'id' => $product->category->id,
+                    'name' => $product->category->name,
+                ],
+                'min_price' => $minPrice,
+                'max_price' => $maxPrice,
+                'thumbnail' => $product->thumbnail,
+            ];
+        });
 
         return response()->json([
-            'data' => $products,
-        ],201);
-
+            'data' => $formattedProducts,
+            'current_page' => $products->currentPage(),
+            'last_page' => $products->lastPage(),
+            'per_page' => $products->perPage(),
+            'total' => $products->total(),
+        ], 200);
     }
 
 
-    public function getProductCate(int $id){
 
+
+    public function getProductAllWithDetail(FilterProductRequest $request)
+    {
+        $perPage = $request->input('per_page', 10);
+
+        $products = Product::with(['productDetails.images', 'category', 'productDetails.size'])
+            ->Filter($request)->paginate($perPage);
+
+        $formattedProducts = collect($products->items())->map(function ($product) {
+            return [
+                'id' => $product->id,
+                'name' => $product->name,
+                'thumbnail' => $product->thumbnail,
+                'status' => $product->status,
+                'category' => [
+                    'id' => $product->category->id,
+                    'name' => $product->category->name,
+                    'image' => $product->category->image
+                ],
+                'product_details' => collect($product->productDetails)->map(function ($detail) {
+                    return [
+                        'id' => $detail->id,
+                        'size' => [
+                            'id' => $detail->size->id,
+                            'name' => $detail->size->name,
+                        ],
+                        'price' => $detail->price,
+                        'quantity' => $detail->quantity,
+                        'images' => collect($detail->images)->map(function ($image) {
+                            return [
+                                'id' => $image->id,
+                                'name' => $image->name
+                            ];
+                        })
+                    ];
+                })
+            ];
+        });
+
+
+        return response()->json([
+            'data' => $formattedProducts,
+
+            'total' => $products->total(),
+            'per_page' => $products->perPage(),
+            'current_page' => $products->currentPage(),
+            'last_page' => $products->lastPage(),
+        ], 200);
+    }
+
+
+
+
+    public function getProductWithDetailByID(int $id)
+    {
+        $product = Product::with(['productDetails.size', 'productDetails.images', 'category'])
+            ->find($id);
+
+        if ($product) {
+            $data = [
+                'id' => $product->id,
+                'name' => $product->name,
+                'thumbnail' => $product->thumbnail,
+                'status' => $product->status,
+                'category' => [
+                    'id' => $product->category->id,
+                    'name' => $product->category->name,
+                    'image' => $product->category->image,
+                ],
+                'product_details' => $product->productDetails->map(function ($detail) {
+                    return [
+                        'id' => $detail->id,
+                        'price' => $detail->price,
+                        'quantity' => $detail->quantity,
+                        'size' => [
+                            'id' => $detail->size->id,
+                            'name' => $detail->size->name,
+                        ],
+                        'images' => $detail->images->map(function ($image) {
+                            return [
+                                'id' => $image->id,
+                                'name' => $image->name,
+                            ];
+                        }),
+                    ];
+                }),
+            ];
+
+            return response()->json([
+                'data' => $data,
+            ], 200);
+        } else {
+            return response()->json([
+                'message' => 'Không tìm thấy sản phẩm với ID này',
+            ], 404);
+        }
+    }
+
+
+
+
+    public function getProductByCate(FilterProductRequest $request,int $id)
+    {
         $products = Product::with(['productDetails.images'])
-        ->where('category_id' ,'=',$id)
-        ->get();
-        
+            ->where('category_id', $id)
+            ->Filter($request)->get();
+
+        if ($products->isEmpty()) {
+            return response()->json([
+                'message' => 'Không có sản phẩm nào trong danh mục này',
+            ], 404);
+        }
+
+
+        $data = $products->map(function ($product) {
+            return [
+                'id' => $product->id,
+                'name' => $product->name,
+                'thumbnail' => $product->thumbnail,
+                'category' => $product->category->name,
+                'product_details' => $product->productDetails->map(function ($detail) {
+                    return [
+                        'id' => $detail->id,
+                        'price' => $detail->price,
+                        'quantity' => $detail->quantity,
+                        'images' => $detail->images->map(function ($image) {
+                            return [
+                                'id' => $image->id,
+                                'name' => $image->name,
+                            ];
+                        }),
+                    ];
+                }),
+            ];
+        });
+
         return response()->json([
-            'data' => $products,
-        ],201);
+            'data' => $data,
+        ], 200);
     }
-
-    
-
 }
