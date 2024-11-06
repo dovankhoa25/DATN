7e114ad8344d5bc4387613a583e9c56fa3008344
dovankhoa25 <khoado432@gmail.com>
2024-11-06 19:02:09 +0700

    bổ sung status

--- app/Http/Controllers/Client/BillOrderController.php
+++ app/Http/Controllers/Client/BillOrderController.php
@@ -61,6 +61,7 @@ class BillOrderController extends Controller
                 'product_detail_id' => $item->product_detail_id,
                 'quantity' => $item->quantity,
                 'price' => $item->price,
+                'status' => 0,
                 'created_at' => now(),
                 'updated_at' => now(),
             ];
