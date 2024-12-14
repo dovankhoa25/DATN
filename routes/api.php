c2d82b54011432689550007c0c5a87a672757d20
dovankhoa25 <khoado432@gmail.com>
2024-12-14 08:27:52 +0700

    toi uu

--- routes/api.php
+++ routes/api.php
@@ -72,7 +72,7 @@ Route::post('pay_check', [TransactionController::class, 'webhook']);
 
 Route::prefix('shipper')->middleware(['auth', 'checkRole:qtv,admin,shipper'])->group(function () {
     Route::get('bill', [ShipperController::class, 'listBill'])->middleware('auth', 'checkRole:shipper,admin,qtv');
-    Route::put('updateShippingStatus', [ShipperController::class, 'updateShippingStatus']);
+    Route::put('updateShippingStatus/{id}', [ShipperController::class, 'updateShippingStatus']);
     Route::put('retryShipping', [ShipperController::class, 'retryShipping']);
 });
 
